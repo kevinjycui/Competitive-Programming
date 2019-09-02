4cdb1e285685921844af5da04704d1c44e6620ca
Kevin JY Cui <info@kevincui.dev>
2019-09-02 10:15:22 -0400

    add ccc09j1

---
+++ Java/CCC/ccc09j1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class ccc09j1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = sc.nextLine();
+        String s = "9780921418"+a+b+c;
+        int count = 0;
+        for (int i=0; i<s.length(); i++){
+            count += Integer.parseInt(Character.toString(s.charAt(i)))*((i%2)*2+1);
+        }
+        System.out.println("The 1-3-sum is "+count);
+    }
+}
