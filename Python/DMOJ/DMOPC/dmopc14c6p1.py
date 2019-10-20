3fd92743ec62cc5771691268f675b119eb4c1754
Kevin Cui JY <kevincui1230@gmail.com>
2019-10-20 16:13:33 -0500

    Revert "DMOPC 14 C2 P4"
    This reverts commit 96a60d9c7633055d42a0ec35f6c60e0abd5414b9.

--- Python/DMOJ/DMOPC/dmopc14c6p1.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-a = []
-for i in range(n):
-    a.append(input())
-if a[a.index('Bessarion')-1] in ['Leslie', 'Bayview'] and a[a.index('Bessarion')+1] in ['Leslie', 'Bayview']:
-    print('Y')
-else:
-    print('N')
--- Python/DMOJ/pwindsor18p1.py
+++
@@ -1,5 +0,0 @@
-s = input()
-if 'java' in s:
-    print(s.index('java'))
-else:
-    print(len(s))
