63a009d8b2cf976954ab27edfa5271198864af49
kevinjycui <info@kevincui.dev>
2020-06-18 20:30:31 -0400

    Graph theory on CF

---
+++ C++/Codeforces/Codeforces Round/Codeforces Round Div 3/cf1006E.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define MAXN 200001
+
+using namespace std;
+
+vector<int> adj[MAXN];
+vector<int> order;
+int children[MAXN];
+
+int dfs(int node) {
+    order.push_back(node);
+    children[node] = 1;
+    for (int next:adj[node])
+        children[node] += dfs(next);
+    return children[node];
+}
+
+int main() {
+
+    int n, q; cin >> n >> q;
+
+    for (int i=2; i<=n; i++) {
+        int a; cin >> a;
+        adj[a].push_back(i);
+    }
+
+    dfs(1);
+
+    for (int i=0; i<q; i++) {
+        int u, k; cin >> u >> k;
+        if (k == 1) {
+            cout << u << endl;
+            continue;
+        }
+        if (k <= children[u]) {
+            int index = find(order.begin(), order.end(), u) - order.begin();
+            cout << (index+k-1 < order.size() ? order[index+k-1]:-1) << endl;
+        }
+        else cout << -1 << endl;
+    }
+
+}
